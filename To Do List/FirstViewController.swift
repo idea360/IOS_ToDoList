63cc20d0cb5284bf9d446f81f92110a809e1ec5d
Chris <idea360@hotmail.com>
2015-10-25 11:26:17 -0700

    Initial Commit

---
+++ To Do List/FirstViewController.swift
@@ -0,0 +1,47 @@
+//
+//  FirstViewController.swift
+//  To Do List
+//
+//  Created by Christopher on 10/24/15.
+//  Copyright © 2015 Idea360. All rights reserved.
+//
+
+import UIKit
+
+var toDoList = [String]()
+
+class FirstViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet weak var toDoListTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return toDoList.count
+    }
+    
+   
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = toDoList[indexPath.row]
+        
+        return cell
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        toDoListTable.reloadData()
+    }
+    
+
+
+}
+
---
+++ To Do List/SecondViewController.swift
@@ -0,0 +1,59 @@
+//
+//  SecondViewController.swift
+//  To Do List
+//
+//  Created by Christopher on 10/24/15.
+//  Copyright © 2015 Idea360. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func addButton(sender: AnyObject) {
+        
+        if let item: String = textField.text {
+            if(item.characters.count > 1){
+                toDoList.append(item)
+            } else {
+                errorMessage("Note title cannot be blank")
+            }
+        }
+        else {
+            errorMessage("Failed to add note.")
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func errorMessage(message: String){
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        let cancelAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+        alert.addAction(cancelAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+
+}
+
